f6bf74d715136a6fefbaf609908a36b1729c3f6d
Krish Kalvani <k4spidey@gmail.com>
2024-02-20 08:58:17 +0400

    initial commit

---
+++ products.js
@@ -0,0 +1,49 @@
+let products= [
+    {
+    id: 1001,
+    title:"Cat Food, 25lb bag",
+    description: "A 25 pound bag of <em>irresistible</em>,"+"organic goodness for your cat",
+    price:2000,
+    image:"images/catFood.jpg",
+    availableInventory: 10,
+    rating:3
+},
+{
+    id:1002,
+    title:"dog food",
+    description: "food for dogs",
+    price: 3000,
+    image:"images/dogFood.jpg",
+    availableInventory: 10,
+    rating: 4
+},
+{
+    id:1003,
+    title:"horse food",
+    description: "food for horse",
+    price: 4000,
+    image:"images/horseFood.jpg",
+    availableInventory: 10,
+    rating: 3
+},
+{
+    id:1004,
+    title:"Kitty litter",
+    description: "a spacious environment for your pet cat to relieve themselves",
+    price: 200,
+    image:"images/kittyLitter.jpg",
+    availableInventory: 10,
+    rating: 2
+
+},
+{
+    id:1005,
+    title:"Dog collar",
+    description: "for naming your dog",
+    price: 300,
+    image:"images/dogCollar.jpg",
+    availableInventory: 10,
+    rating: 2
+
+}
+]
